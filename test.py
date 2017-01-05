329540a83b15944c50034442c7b1c588d0cfad24
tkjelsrud <tkjelsrud@gmail.com>
2017-01-05 15:09:42 +0100

    Updated test.py

--- test.py
+++ test.py
@@ -26,6 +26,12 @@
 # np.setTemplate("BD", "101?") # first and third beat
 # np.setTemplate("SD", "0???") # nothing on the first step
 # np.swing("sine", "0101", 0.5, doodle.getMicroRes()) # Factor of swing, guide, amount and speed (size of cycle)
+# # Some ideas
+# Destinations: timing (default), velocity, duration, repeating
+# Type of modifiers: sine (bipolar), pos sine (just the positive values), abssine, tri, random
+#
+# Don't try everything at once - the modifier is all that needs to be rated, the note grid/template can be static!
+#
 # np.randomize()
 # np.drawInBar(bar)
 
@@ -40,6 +46,10 @@
 #   What swing guides are rated +1 or -1
 
 #
-# BD 1 0 1 0
-# SD 0 1 0 1
+## Separate thread responsisble for
+# Start playback
+# Suspend playback
+# While playback
+#   using timing, play notes
+#   mido.Message('note_on', note=100, velocity=3, time=6.2)
 #
